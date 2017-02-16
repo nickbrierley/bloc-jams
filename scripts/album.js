563e1db04065626d7362022e5228da1ebfa7fa6f
Nick Brierley <Nick Brierley>
2017-02-16 10:19:47 -0800

    checkpoint21: fixed play pause and seekbar

--- scripts/album.js
+++ scripts/album.js
@@ -98,6 +98,24 @@ var clickHandler = function() {
     }
 };
 
+var togglePlayFromPlayerBar = function() {
+    var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    
+    if(!currentSoundFile){
+        return;
+    } 
+    
+    if(currentSoundFile.isPaused()) {
+        $(currentlyPlayingCell).html(pauseButtonTemplate);
+        $playPause.html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else { 
+        $(currentlyPlayingCell).html(playButtonTemplate);
+        $playPause.html(playerBarPlayButton);
+        currentSoundFile.pause(); 
+    }
+};
+
 var setCurrentAlbum = function(album) {
     currentAlbum = album;
     var $albumTitle = $('.album-view-title');
@@ -259,10 +277,11 @@ var currentSoundFile = null;
 var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
-
+var $playPause = $('.main-controls .play-pause')
 $(document).ready(function() {
     setCurrentAlbum(albumPat);
     setupSeekBars();
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPause.click(togglePlayFromPlayerBar)
  });
